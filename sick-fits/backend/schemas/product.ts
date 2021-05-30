a409117e27cbbda47774b474d939f38997ad0ea6
Luke Bennett <hello@lukebennett.com.au>
2021-05-30 20:37:02 +1000

    Add slug field to products

--- sick-fits/backend/schemas/product.ts
+++ sick-fits/backend/schemas/product.ts
@@ -1,11 +1,24 @@
 import { integer, relationship, select, text } from "@keystone-next/fields";
 import { list } from "@keystone-next/keystone/schema";
+import slugify from "@sindresorhus/slugify";
 
 export const Product = list({
   // TODO:
   // access:
   fields: {
     name: text({ isRequired: true }),
+    slug: text({
+      isRequired: true,
+      isUnique: true,
+      label: "Slug (unique identifier of the product)",
+      hooks: {
+        beforeChange: async ({ originalInput, resolvedData }) => {
+          const slug = slugify(originalInput.slug);
+          // eslint-disable-next-line no-param-reassign
+          resolvedData.slug = slug;
+        },
+      },
+    }),
     description: text({
       ui: {
         displayMode: "textarea",
