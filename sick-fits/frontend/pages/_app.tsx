51f2a641bedd07d1b5f2c815bbcdcdedbd921b97
Luke Bennett <hello@lukebennett.com.au>
2021-05-30 22:12:54 +1000

    Add ApolloProvider

--- sick-fits/frontend/pages/_app.tsx
+++ sick-fits/frontend/pages/_app.tsx
@@ -3,17 +3,38 @@ import { Layout } from "../components/layout";
 import Router from "next/router";
 import NProgress from "nprogress";
 import "../styles/global.css";
+import { ApolloProvider } from "@apollo/client";
+import type { ApolloClient, NormalizedCacheObject } from "@apollo/client";
+import withData from "../lib/with-data";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
-function App({ Component, pageProps }: AppProps) {
+function App({
+  Component,
+  pageProps,
+  apollo,
+}: AppProps & { apollo: ApolloClient<NormalizedCacheObject> }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={apollo}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
   );
 }
 
-export default App;
+App.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {
+    query: undefined,
+  };
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+// @ts-ignore
+export default withData(App);
